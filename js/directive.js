6489c66acdeb6d0fd08261ec36e8f1c99226beb1
unshame <mrunshame@gmail.com>
2017-12-18 16:41:25 +0300

    Цвета лейблов при выборе

--- js/directive.js
+++ js/directive.js
@@ -13,3 +13,25 @@ app.directive('getFocus', function($timeout) {
 		}
 	};
 });
+
+// Добавляет цвета лейблам при выборе
+app.directive('labelColors', function () {
+  return {
+    require: 'select',
+    link: function(scope, elem, attrs, ngSelect) {
+        scope.$watch('labels', function(items) {
+            scope.labels.forEach(function(label) {
+                var options = elem.find('option');
+                // По какой-то причине 'option[label]' не срабатывает
+                for(var i = 0; i < options.length; i++){
+                    var option = options[i];
+                    if(option.label == label.key){ 
+                        angular.element(option).addClass(label.color);
+                        break;
+                    }
+                }
+            });
+        }, true);
+    }
+  };
+});
